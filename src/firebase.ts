3b46bb841734fb2f2ffd3bbcaa3c855f9f08015d
rchaves77 <140459080+rchaves77@users.noreply.github.com>
2026-06-08 21:10:04 -0500

    ajuste chave API

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYFKI7w7Lhe4ljvrO13s7e8cERMsBVr2Q",
-  authDomain: "chaves-estacio.firebaseapp.com",
-  databaseURL: "https://chaves-estacio-default-rtdb.firebaseio.com",
-  projectId: "chaves-estacio",
-  storageBucket: "chaves-estacio.firebasestorage.app",
-  messagingSenderId: "1052463955885",
-  appId: "1:1052463955885:web:32abff964d95be9f95ab24",
-  measurementId: "G-CESXMKVRE0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
