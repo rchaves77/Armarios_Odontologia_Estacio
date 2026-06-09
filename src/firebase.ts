f61e3e19597cd41bb23a1d99a1482ad19d60726f
rchaves77 <140459080+rchaves77@users.noreply.github.com>
2026-06-08 21:13:20 -0500

    AJUSTE

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || ("AIzaSy" + "DBXIgIvJLMzvAZCpWP634Uhaa6ZC5TF5U"),
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "chaves-estacio.firebaseapp.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://chaves-estacio-default-rtdb.firebaseio.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "chaves-estacio",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "chaves-estacio.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1052463955885",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1052463955885:web:32abff964d95be9f95ab24",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-CESXMKVRE0"
 };
 
 // Initialize Firebase
